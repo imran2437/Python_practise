1bc7e9bad60d6bd8e6dea1edb478f73e14c6fb70
imran2437 <alimran689@gmail.com>
2023-08-21 19:11:39 +0600

    algo

---
+++ Algorithm/binary_search.py
@@ -0,0 +1,23 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2  # Calculate the middle index
+
+        if arr[mid] == target:
+            return mid  # Found the target, return its index
+        elif arr[mid] < target:
+            left = mid + 1  # Target is in the right half
+        else:
+            right = mid - 1  # Target is in the left half
+
+    return -1  # Target not found in the array
+
+# Example usage:
+my_list = [1, 3, 5, 7, 9, 11, 13, 15]
+target = 7
+result = binary_search(my_list, target)
+if result != -1:
+    print(f"Target {target} found at index {result}")
+else:
+    print("Target not found in the list.")
